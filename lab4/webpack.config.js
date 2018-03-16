8a910a0b03f91841b327685b2215265e11ea009a
Vincent Boiteau-Robert <vincent@decksign.com>
2018-03-16 09:12:06 -0400

    Add lab4 start

---
+++ lab4/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './client/index.html',
+    filename: 'index.html',
+    inject: false
+});
+
+const getPath = p => path.resolve(path.join(__dirname, p));
+
+module.exports = {
+    entry: ['babel-polyfill', './client/index.js'],
+    output: {
+        path: path.resolve('./dist'),
+        filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [HtmlWebpackPluginConfig]
+};
