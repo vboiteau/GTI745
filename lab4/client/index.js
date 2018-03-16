c6c2c52e4cb7dcd3dbacfd6ee662c8842dbffc22
Vincent Boiteau-Robert <vincent@decksign.com>
2018-03-16 11:41:46 -0400

    Add graph

--- lab4/client/index.js
+++ lab4/client/index.js
@@ -3,43 +3,52 @@ import Artist from './data/Artist.txt';
 import ArtistInf from './data/Artist_influenced_by.txt';
 
 const svg = d3.select("svg");
-const width = +svg.attr("width");
-const height = +svg.attr("height");
+const width = document.querySelector('svg').clientWidth;
+const height = document.querySelector('svg').clientHeight;
+
+console.log(width, height, document);
 
 const color = d3.scaleOrdinal(d3.schemeCategory20);
 const simulation = d3.forceSimulation()
     .force("link", d3.forceLink().id(function(d) {
-        return d['# pgid'];
+        return d.id;
     }))
     .force("charge", d3.forceManyBody())
     .force("center", d3.forceCenter(width / 2, height / 2));
 
-d3.csv(Artist, (err, artists) => {
-    d3.csv(ArtistInf, (err, influences) => {
-        console.log({ artists, influences });
+d3.csv(Artist, artist => Object.assign(artist, {
+    id: artist['# pgid']
+}), (err, artists) => {
+    const artistIds = artists.map(({ id }) => id);
+    d3.csv(ArtistInf, influence => Object.assign(influence, {
+        source: influence['# pgid'],
+        target: influence[' influencer_pgid']
+    }), (err, influences) => {
+        influences = influences.filter(({ source, target }) => artistIds.includes(source) && artistIds.includes(target));
         const link = svg.append('g')
             .attr('class', 'links')
             .selectAll('line')
             .data(influences)
             .enter()
             .append('line')
-            .attr('stroke-width', function(d) { return Math.sqrt(d.value);});
+            .attr('stroke-width', function(d) { 
+                return 2;
+            });
 
-        var node = svg.append('g')
+        const node = svg.append('g')
             .attr('class', 'nodes')
+            .selectAll('circle')
             .data(artists)
-            .enter()
-            .append('circle')
+            .enter().append('circle')
             .attr('r', 5)
-            .attr('fill', d => 'black');
-
-        node.attr('id', d => d['# pgid']);
+            .attr('fill', d => 'black')
+            .call(d3.drag()
+                .on('start', dragstarted)
+                .on('drag', dragged)
+                .on('end', dragended));
 
         node.append('title')
-            .text(d => d['# pgid']);
-
-        link.append('title')
-            .text(d => d['# pgid']);
+            .text(d => d.id);
 
         simulation
             .nodes(artists)
@@ -51,14 +60,17 @@ d3.csv(Artist, (err, artists) => {
 
         function ticked() {
             link
-                .attr("x1", function(d) { return d['# pgid'].x; })
-                .attr("y1", function(d) { return d['# pgid'].y; })
-                .attr("x2", d => d[' influencer_pgid'].x)
-                .attr("y2", d => d[' influencer_pgid'].y);
+                .attr("x1", function(d) {
+                    return d.source.x;
+                })
+                .attr("y1", function(d) {
+                    return d.source.y;
+                })
+                .attr("x2", d => d.target.x)
+                .attr("y2", d => d.target.y);
 
             node
                 .attr("cx", function(d) { 
-                    console.log(d);
                     return d.x;
                 })
                 .attr("cy", function(d) {
@@ -67,3 +79,21 @@ d3.csv(Artist, (err, artists) => {
         }
     });
 });
+
+
+function dragstarted(d) {
+  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+  d.fx = d.x;
+  d.fy = d.y;
+}
+
+function dragged(d) {
+  d.fx = d3.event.x;
+  d.fy = d3.event.y;
+}
+
+function dragended(d) {
+  if (!d3.event.active) simulation.alphaTarget(0);
+  d.fx = null;
+  d.fy = null;
+}
