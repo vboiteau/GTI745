73026c8afd60459b82bd41954c97fd6455ca005b
Vincent Boiteau-Robert <vincent@decksign.com>
2018-03-23 10:38:44 -0400

    Add data getter

---
+++ lab4/client/dataGetter.js
@@ -0,0 +1,63 @@
+import * as d3 from 'd3';
+import Artist from './data/Artist.txt';
+import ArtistInf from './data/Artist_influenced_by.txt';
+
+let data = null;
+
+function getAmountOfNodes(amount, influences, artists){
+
+    var inf = [];
+    var art = [];
+    var count = 0;
+
+    influences.forEach(influence => {
+
+        if(count < amount){
+
+            //For actual pgid of the artist
+            if(!art.map(a => {return a.pgid}).includes(influence.pgid)){
+                art.push(artists.find(a => {return a.pgid === influence.pgid}));
+                count++;
+            }
+            //For the influencer's pgid
+            if(!art.map(a => {return a.pgid}).includes(influence.influencer_pgid)){
+                //Finds the influencers ID inside the list
+                art.push(artists.find(a => {return a.pgid === influence.influencer_pgid}));
+                count++;
+            }
+
+            //We add the influence link
+            inf.push(influence);
+        }
+
+    });
+
+    return {
+        "artists": art,
+        "influences": inf
+    }
+
+}
+
+export default () => new Promise(resolve => {
+    if (data) {
+        resolve(data);
+    }
+    d3.csv(Artist, artist => Object.assign(artist, {
+        id: artist['pgid']
+    }), (err, artists) => {
+        const artistIds = artists.map(({ id }) => id);
+        d3.csv(ArtistInf, influence => Object.assign(influence, {
+            source: influence['pgid'],
+            target: influence['influencer_pgid']
+        }), (err, influences) => {
+
+            influences = influences.filter(({ source, target }) => artistIds.includes(source) && artistIds.includes(target));
+
+            data = getAmountOfNodes(100, influences, artists);
+            resolve(data);
+        });
+    });
+});
+
+
--- lab4/client/index.js
+++ lab4/client/index.js
@@ -1,67 +1,18 @@
 import * as d3 from 'd3';
-import Artist from './data/Artist.txt';
-import ArtistInf from './data/Artist_influenced_by.txt';
-import { ForceGraph } from './ForceGraph.js'
+import dataGetter from './dataGetter'
+import { ForceGraph } from './ForceGraph'
 
 var svg = d3.select("svg");
 
-d3.csv(Artist, artist => Object.assign(artist, {
-    id: artist['pgid']
-}), (err, artists) => {
-    const artistIds = artists.map(({ id }) => id);
-    d3.csv(ArtistInf, influence => Object.assign(influence, {
-        source: influence['pgid'],
-        target: influence['influencer_pgid']
-    }), (err, influences) => {
 
-        influences = influences.filter(({ source, target }) => artistIds.includes(source) && artistIds.includes(target));
-
-        var reduced = getAmountOfNodes(100, influences, artists);
-
-        var forceGraph = new ForceGraph(reduced.artists, reduced.influences, svg);
-
+dataGetter()
+    .then(data => {
+        var forceGraph = new ForceGraph(data.artists, data.influences, svg);
         forceGraph.init();
 
         //setTimeout(function(){forceGraph.disposeInCircle()}, 1000)
-        
-    });
-});
-
-//Gets X amount of nodes to display with their influence links
-function getAmountOfNodes(amount, influences, artists){
-
-    var inf = [];
-    var art = [];
-    var count = 0;
-
-    influences.forEach(influence => {
-
-        if(count < amount){
-
-            //For actual pgid of the artist
-            if(!art.map(a => {return a.pgid}).includes(influence.pgid)){
-                art.push(artists.find(a => {return a.pgid === influence.pgid}));
-                count++;
-            }
-            //For the influencer's pgid
-            if(!art.map(a => {return a.pgid}).includes(influence.influencer_pgid)){
-                //Finds the influencers ID inside the list
-                art.push(artists.find(a => {return a.pgid === influence.influencer_pgid}));
-                count++;
-            }
-
-            //We add the influence link
-            inf.push(influence);
-        }
-
     });
 
-    return {
-        "artists": art,
-        "influences": inf
-    }
-
-}
     
 window.onresize = function(){
 
